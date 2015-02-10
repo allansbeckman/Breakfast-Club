7cff931c2a989670a3eaafc8ada63a3dcffdf655
Andrew Martin del Campo <a.md55@ymail.com>
2015-02-09 20:08:29 -0800

    Execution Test 1
    -created all hi-fidelity mock ups and combined them

--- EmbraceMobile/www/js/index_user_scripts.js
+++ EmbraceMobile/www/js/index_user_scripts.js
@@ -1,13 +1,18 @@
 (function()
 {
+    console.log("out");
+     $.ui.useOSThemes=false;
+    
+$(window).bind('setup', function() {
+   console.log("in");
+    $.ui.useOSThemes=false;
+});
  "use strict";
  /*
    hook up event handlers 
  */
  function register_event_handlers()
  {
-    
-    
      /* button  Button1 */
     $(document).on("click", ".uib_w_10", function(evt)
     {
@@ -25,6 +30,12 @@
     $(document).on("click", ".uib_w_18", function(evt)
     {
          activate_subpage("#page_70_42"); 
+    });
+    
+        /* button  Send */
+    $(document).on("click", ".uib_w_27", function(evt)
+    {
+      alert("message sent");
     });
     
     }
