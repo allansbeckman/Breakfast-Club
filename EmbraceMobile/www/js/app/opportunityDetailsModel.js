58d2a57ce00567be74286de7412908ece75e88d7
allansbeckman <allansbeckman@gmail.com>
2015-02-27 16:18:09 -0800

    Revert "opportunity_details"
    This reverts commit 1757fb710ab87157abe63ec3497c585f85a7f86c.

--- EmbraceMobile/www/js/app/opportunityDetailsModel.js
+++
@@ -1,6 +0,0 @@
-define(['app/opportunityDetails'], function(opportunityDetails){
-  var OpportunityDetails = Backbone.Collection.extend({
-  model: opportunityDetails
-  });
-  return new OpportunityDetails();
-});
