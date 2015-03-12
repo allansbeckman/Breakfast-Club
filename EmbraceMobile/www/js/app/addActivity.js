2b438f9ae2519cfec312de5e92b7e12789672a54
Jose <jromovel@uci.edu>
2015-03-12 16:31:02 -0700

    Saving current work
    trying to create a poplist of property user can choose from.
    this will replace the value of the property address.
    then feed to activity create screen.

---
+++ EmbraceMobile/www/js/app/addActivity.js
@@ -0,0 +1,27 @@
+define([],function(){
+        activityPropertySearchButton.addEventListener('click',function(){
+            searchProperty(propertyActivity.value);
+        });
+       activitySaveButton.addEventListener('click',function(){
+            addActivity();
+        });
+
+
+    function searchProperty(propertyWords){
+        url = "htpp://54.69.150.79:8080/embrace2/property/search";
+        jQuery.ajax({
+            type:"POST",
+            url: url,
+            contentType:"application/json",
+            dataType: "json",
+            data:{"propertyAddress":propertyWords,"isMobile":"Y"},
+            success: function(json){
+                $.each(json,function(i,value){
+                    $("@propertySearchResult").append($('<option>').text(value).attr('value',value));});
+                $("#propertySearchResultPopup").show();
+            },
+            error: function(jqXHR, status){
+                alert("invalid property address");}    
+        });
+    }
+});
